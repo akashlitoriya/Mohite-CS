eb3e77a449e9aa8860c9b4374e5cccb22db4a247
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2023-10-15 11:15:24 +0530

    Responsiveness added in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../data/logo.png'
-
+import {ImCross} from 'react-icons/im'
+import {AiOutlineMenu} from 'react-icons/ai'
 const Navbar = () => {
-  
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>
-      <div className='lg:w-[1200px] h-14  mx-auto flex justify-between items-center'>
-        <div className='flex gap-5 justify-center items-center'>
-            <div>
-                <img src = {logo} alt='logo' className='h-[48px]'/>
-            </div>
-            <div>
-                <ul className='flex gap-3 text-base font-bold'>
-                    <li>Solutions</li>
-                    <li>Features</li>
-                    <li>Blogs</li>
-                    <li>About</li>
-                </ul>
-            </div>
+    <div className=''>
+      <div className='lg:w-[1200px] h-14  mx-auto flex items-center justify-between md:justify-normal gap-6 relative p-5'>
+        <div>
+          <img src = {logo} alt='logo' className='h-[48px]'/>
         </div>
+        <div className={`flex gap-5 w-full md:justify-between bg-slate-400 flex-col md:flex-row items-center z-30 h-screen md:h-fit justify-center text-center md:static transition-all duration-300 ${toggleMenu? "absolute top-0 left-0" : "absolute top-0 left-0 -translate-y-full md:translate-y-0 md:bg-white"}`}>
+          <div>
+            <ul className='flex gap-3 flex-col md:flex-row text-base font-bold'>
+              <li>Solutions</li>
+              <li>Features</li>
+              <li>Blogs</li>
+              <li>About</li>
+            </ul>
+          </div>
 
-        <div className='flex gap-3 h-[48px]'>
-          <button className='border-[1.5px] border-primary text-base font-bold text-primary px-[20px] py-[10px] rounded-[10px]'>Login</button>
-          <button className=' bg-primary text-base font-bold text-white px-[20px] py-[10px] rounded-[10px]'>Register</button>
+          <div className='flex flex-col md:flex-row gap-3 h-[48px]'>
+            <button className='border-[1.5px] border-primary text-base font-bold text-primary px-[20px] py-[10px] rounded-[10px]'>Login</button>
+            <button className=' bg-primary text-base font-bold text-white px-[20px] py-[10px] rounded-[10px]'>Register</button>
+          </div>
+          
         </div>
+        <button className={`block z-50 md:hidden`} onClick={()=> setToggleMenu(!toggleMenu)}>
+            {
+              !toggleMenu? (<AiOutlineMenu className='font-bold text-lg'/>):(<ImCross />)
+            }
+        </button>
+        
 
       </div>
     </div>
